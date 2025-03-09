c4816f8eb63a2a8fcc0c3f3d13d73e69c78d57ed
Robert Collins <collinsrj@hotmail.com>
2025-03-09 17:21:11 +0000

    First commit

---
+++ src/main/java/com/collinsrj/referenceproject/ReferenceprojectApplication.java
@@ -0,0 +1,13 @@
+package com.collinsrj.referenceproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReferenceprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReferenceprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/collinsrj/referenceproject/ReferenceprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collinsrj.referenceproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReferenceprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
